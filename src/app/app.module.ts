a1cf62de6565b6b2985d8385c5b27a1f5c17683f
Uriel Aispuro <urielalexis64@gmail.com>
2020-11-12 18:18:21 -0700

    heroe and search routes with params added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { NgModule } from '@angular/core';
 import { APP_ROUTING } from './app.routes';
 
 //Services
+import { HeroesService } from './services/heroes.service';
 
 //Components
 import { AppComponent } from './app.component';
@@ -12,6 +13,8 @@ import { NavbarComponent } from './components/shared/navbar/navbar.component';
 import { HomeComponent } from './components/home/home.component';
 import { AboutComponent } from './components/about/about.component';
 import { HeroesComponent } from './components/heroes/heroes.component';
+import { HeroeComponent } from './components/heroe/heroe.component';
+import { SearchComponent } from './components/search/search.component';
 
 @NgModule({
   declarations: [
@@ -20,9 +23,11 @@ import { HeroesComponent } from './components/heroes/heroes.component';
     HomeComponent,
     AboutComponent,
     HeroesComponent,
+    HeroeComponent,
+    SearchComponent,
   ],
   imports: [BrowserModule, APP_ROUTING],
-  providers: [],
+  providers: [HeroesService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,9 @@
 import { Routes, RouterModule } from '@angular/router';
-import { NgModule } from '@angular/core';
-
 import { HomeComponent } from './components/home/home.component';
 import { HeroesComponent } from './components/heroes/heroes.component';
 import { AboutComponent } from './components/about/about.component';
+import { HeroeComponent } from './components/heroe/heroe.component';
+import { SearchComponent } from './components/search/search.component';
 
 const APP_ROUTES: Routes = [
   {
@@ -13,6 +13,14 @@ const APP_ROUTES: Routes = [
   {
     path: 'heroes',
     component: HeroesComponent,
+  },
+  {
+    path: 'heroe/:id',
+    component: HeroeComponent,
+  },
+  {
+    path: 'search/:filter',
+    component: SearchComponent,
   },
   {
     path: 'about',
