2b1d37231a40bc6b5622377654a19c754d305a03
Uriel Aispuro <urielalexis64@gmail.com>
2020-11-12 18:20:33 -0700

    search heroes functionality added

--- src/app/components/shared/navbar/navbar.component.ts
+++ src/app/components/shared/navbar/navbar.component.ts
@@ -1,11 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
 })
-export class NavbarComponent implements OnInit {
-  constructor() {}
+export class NavbarComponent {
+  constructor(private _router: Router) {}
 
-  ngOnInit(): void {}
+  searchHero(term: string, event: any): void {
+    event.preventDefault();
+    this._router.navigate(['/search', term]);
+  }
 }
